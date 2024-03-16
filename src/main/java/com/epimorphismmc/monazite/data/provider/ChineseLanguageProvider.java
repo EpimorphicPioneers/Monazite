517873bff51c6a79828dacf85c5f671e56624b08
Gate Guardian <xtzcy76040108@163.com>
2024-03-16 17:07:36 +0800

    Update Lang

--- src/main/java/com/epimorphismmc/monazite/data/provider/ChineseLanguageProvider.java
+++ src/main/java/com/epimorphismmc/monazite/data/provider/ChineseLanguageProvider.java
@@ -16,5 +16,14 @@ public class ChineseLanguageProvider extends LanguageProvider {
         add("monazite.fluid.outputs", "流体制作中:");
         add("config.jade.plugin_monazite.recipe_info_item_output", "配方物品显示");
         add("config.jade.plugin_monazite.recipe_info_fluid_output", "配方流体显示");
+        add("key.categories.monazite", "Monazite");
+        add("key.toggleConciseMode", "开/关简洁模式");
+        add("config.monazite.option.topInformation", "顶部信息");
+        add("config.monazite.option.displayItemOutputs", "显示物品输出");
+        add("config.monazite.option.displayItemName", "显示物品名称");
+        add("config.monazite.option.displayFluidOutputs", "显示流体输出");
+        add("config.monazite.option.displayFluidName", "显示流体名称");
+        add("config.monazite.option.conciseMode", "简洁模式");
+        add("config.monazite.option.itemsPerLine", "每行物品数");
     }
 }
--- src/main/java/com/epimorphismmc/monazite/data/provider/EnglishLanguageProvider.java
+++ src/main/java/com/epimorphismmc/monazite/data/provider/EnglishLanguageProvider.java
@@ -16,5 +16,14 @@ public class EnglishLanguageProvider extends LanguageProvider {
         add("monazite.fluid.outputs", "Fluid Crafting:");
         add("config.jade.plugin_monazite.recipe_info_item_output", "Recipe Items Display");
         add("config.jade.plugin_monazite.recipe_info_fluid_output", "Recipe Fluids Display");
+        add("key.categories.monazite", "Monazite");
+        add("key.toggleConciseMode", "Toggle Concise Mode");
+        add("config.monazite.option.topInformation", "Top Information");
+        add("config.monazite.option.displayItemOutputs", "Display Item Outputs");
+        add("config.monazite.option.displayItemName", "Display Item Name");
+        add("config.monazite.option.displayFluidOutputs", "Display Fluid Outputs");
+        add("config.monazite.option.displayFluidName", "Display Fluid Name");
+        add("config.monazite.option.conciseMode", "Concise Mode");
+        add("config.monazite.option.itemsPerLine", "Items Per Line");
     }
 }
