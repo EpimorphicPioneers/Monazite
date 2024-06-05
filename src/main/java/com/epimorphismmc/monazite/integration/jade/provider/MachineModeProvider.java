c03fc7b9c62f43f6ea74c85c75f15105c187e1f1
DancingSnow <1121149616@qq.com>
2024-06-05 22:06:32 +0800

    修改机器模式显示样式，按下shift显示所有模式

--- src/main/java/com/epimorphismmc/monazite/integration/jade/provider/MachineModeProvider.java
+++ src/main/java/com/epimorphismmc/monazite/integration/jade/provider/MachineModeProvider.java
@@ -4,8 +4,12 @@ import com.epimorphismmc.monazite.Monazite;
 import com.gregtechceu.gtceu.api.blockentity.MetaMachineBlockEntity;
 import com.gregtechceu.gtceu.api.machine.feature.IRecipeLogicMachine;
 import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
+import net.minecraft.ChatFormatting;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.StringTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.Nullable;
 import snownee.jade.api.BlockAccessor;
@@ -19,9 +23,27 @@ public enum MachineModeProvider implements IBlockComponentProvider, IServerDataP
 
     @Override
     public void appendTooltip(ITooltip iTooltip, BlockAccessor blockAccessor, IPluginConfig iPluginConfig) {
-        if (blockAccessor.getServerData().contains("RecipeType")) {
-            ResourceLocation recipeType = new ResourceLocation(blockAccessor.getServerData().getString("RecipeType"));
-            iTooltip.add(Component.translatable("monazite.machine_mode", Component.translatable("%s.%s".formatted(recipeType.getNamespace(), recipeType.getPath()))));
+        CompoundTag serverData = blockAccessor.getServerData();
+        if (serverData.contains("RecipeTypes") && serverData.contains("CurrentRecipeType")) {
+            int currentRecipeTypeIndex = serverData.getInt("CurrentRecipeType");
+            ListTag recipeTypesTagList = serverData.getList("RecipeTypes", StringTag.TAG_STRING);
+            if (blockAccessor.showDetails()) {
+                iTooltip.add(Component.translatable("monazite.machine_mode"));
+                for (int i = 0; i < recipeTypesTagList.size(); i++) {
+                    ResourceLocation recipeType = new ResourceLocation(recipeTypesTagList.getString(i));
+                    MutableComponent text;
+                    if (currentRecipeTypeIndex == i) {
+                        text = Component.literal(" > ").withStyle(ChatFormatting.BLUE);
+                    } else {
+                        text = Component.literal("   ");
+                    }
+                    text.append(Component.translatable("%s.%s".formatted(recipeType.getNamespace(), recipeType.getPath())));
+                    iTooltip.add(text);
+                }
+            } else {
+                ResourceLocation recipeType = new ResourceLocation(recipeTypesTagList.getString(currentRecipeTypeIndex));
+                iTooltip.add(Component.translatable("monazite.machine_mode").append(Component.translatable("%s.%s".formatted(recipeType.getNamespace(), recipeType.getPath()))));
+            }
         }
     }
 
@@ -31,7 +53,17 @@ public enum MachineModeProvider implements IBlockComponentProvider, IServerDataP
             @Nullable GTRecipeType[] recipeTypes = blockEntity.getMetaMachine().getDefinition().getRecipeTypes();
             if (recipeTypes.length > 1) {
                 if (blockEntity.getMetaMachine() instanceof IRecipeLogicMachine recipeLogicMachine) {
-                    compoundTag.putString("RecipeType", recipeLogicMachine.getRecipeType().registryName.toString());
+                    ListTag recipeTypesTagList = new ListTag();
+                    GTRecipeType currentRecipeType = recipeLogicMachine.getRecipeType();
+                    int currentRecipeTypeIndex = -1;
+                    for (int i = 0; i < recipeTypes.length; i++) {
+                        if (recipeTypes[i] == currentRecipeType) {
+                            currentRecipeTypeIndex = i;
+                        }
+                        recipeTypesTagList.add(StringTag.valueOf(recipeTypes[i].registryName.toString()));
+                    }
+                    compoundTag.put("RecipeTypes", recipeTypesTagList);
+                    compoundTag.putInt("CurrentRecipeType", currentRecipeTypeIndex);
                 }
             }
         }
